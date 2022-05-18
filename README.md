29c4e3df194e092de6ec090d49ff401dee30ee9d
Ovid <ovid@cpan.org>
2022-05-18 12:35:51 +0200

    Update the docs

--- README.md
+++ README.md
@@ -15,8 +15,12 @@ package My::Names {
       qw(compile Num NonEmptyStr Str PositiveInt ArrayRef);
     use List::Util 'sum';
 
+    # the distinction between `param` and `field` makes it easier to
+    # see which are available to `new`
     param _name   => ( isa => NonEmptyStr, init_arg => 'name' );
     param title   => ( isa => Str,         required => 0 );
+
+    # forbidden in the constructor
     field created => ( isa => PositiveInt, default  => sub { time } );
 
     sub name ($self) {
@@ -40,15 +44,123 @@ package My::Names {
 
 # DESCRIPTION
 
-TODO
+This module is **EXPERIMENTAL**! All features subject to change.
+
+This class attempts to create a "safer" version of Moose that default to
+read-only attributes and is easier to read and write.
+
+It's sort of the equivalent to:
+
+```perl
+package My::Class {
+    use v5.22.0;
+    use Moose;
+    use MooseX::StrictConstructor;
+    use feature 'signatures';
+    no warnings 'experimental::signatures';
+    use namespace::autoclean;
+    use Carp;
+    use mro 'c3';
+
+    ... your code here
+}
+```
+
+It also exports two functions which are similar to Moose `has`: `param` and `field`.
+
+A `param` is a required parameter (defaults may be used). A `field` is not
+allowed to be passed to the constructor.
 
 ## `init_meta`
 
 Internal method setting up exports. Do not call directly.
 
+## `param`
+
+```perl
+param name => ( isa => NonEmptyStr );
+```
+
+A similar function to Moose's `has`. A `param` is required. You may pass it
+to the contructor, or use a `default` or `builder` to supply this value.
+
+The above `param` definition is equivalent to:
+
+```perl
+has name => (
+    is       => 'ro',
+    isa      => NonEmptyStr,
+    required => 1,
+);
+```
+
+If you want a parameter that has no `default` or `builder` and can
+_optionally_ be passed to the constructor, just use `required => 0`.
+
+```perl
+param title => ( isa => Str, required => 0 );
+```
+
+Note that `param`, like `field`, defaults to read-only, `is => 'ro`.
+You can override this:
+
+```perl
+param name => ( is => 'rw', isa => NonEmptyStr );
+```
+
+Otherwise, it behaves like `has`. You can pass in any arguments that `has`
+accepts.
+
+```perl
+# we'll make it private, but allow it to be passed to the constructor
+# as `name`
+param _name   => ( isa => NonEmptyStr, init_arg => 'name' );
+```
+
 ## `field`
 
-Set up our own version of `field()`.
+```perl
+field created => ( isa => PositiveInt, default => sub { time } );
+```
+
+A similar function to Moose's `has`. A `field` is never allowed to be passed
+to the constructor, but you can still use `default` or `builder`, as normal.
+
+The above `field` definition is equivalent to:
+
+```perl
+has created => (
+    is       => 'ro',
+    isa      => PositiveInt,
+    init_arg => undef,        # not allowed in the constructor
+    default  => sub { time },
+);
+```
+
+Note that `field`, like `param`, defaults to read-only, `is => 'ro`.
+You can override this:
+
+```perl
+field some_data => ( is => 'rw', isa => NonEmptyStr );
+```
+
+Otherwise, it behaves like `has`. You can pass in any arguments that `has`
+accepts. However, if you pass in `init_arg`, that will be ignored.
+
+# TODO
+
+```
+# :(
+__PACKAGE__->meta->make_immutable; # we want this to be optional
+```
+
+Try to figure out how to automatically make the class immutable.
+`B::Hooks::EndOfScope` did not work because `param` and `field` fire at
+runtime, not compile-time, and making the class immutable at the end of scope
+fires _before_ `param` and `field` are run.
+
+I thought seriously about making the class mutable in each of those functions
+and immutable after, but hey, we don't need that performance hit.
 
 # AUTHOR
 
--- lib/MooseX/Extreme.pm
+++ lib/MooseX/Extreme.pm
@@ -16,7 +16,7 @@ our $VERSION = '0.01';
 
 Moose::Exporter->setup_import_methods(
     with_meta => [ 'field', 'param' ],
-    as_is     => [ \&carp, \&croak, \&confess ],
+    as_is     => [ \&carp, \&croak ],
     also      => ['Moose'],
 );
 
@@ -40,9 +40,77 @@ sub init_meta {
     mro::set_mro( scalar caller(), 'c3' );
 }
 
+=head2 C<param>
+
+    param name => ( isa => NonEmptyStr );
+
+A similar function to Moose's C<has>. A C<param> is required. You may pass it
+to the contructor, or use a C<default> or C<builder> to supply this value.
+
+The above C<param> definition is equivalent to:
+
+    has name => (
+        is       => 'ro',
+        isa      => NonEmptyStr,
+        required => 1,
+    );
+
+If you want a parameter that has no C<default> or C<builder> and can
+I<optionally> be passed to the constructor, just use C<< required => 0 >>.
+
+    param title => ( isa => Str, required => 0 );
+
+Note that C<param>, like C<field>, defaults to read-only, C<< is => 'ro >>.
+You can override this:
+
+    param name => ( is => 'rw', isa => NonEmptyStr );
+
+Otherwise, it behaves like C<has>. You can pass in any arguments that C<has>
+accepts.
+
+    # we'll make it private, but allow it to be passed to the constructor
+    # as `name`
+    param _name   => ( isa => NonEmptyStr, init_arg => 'name' );
+
+=cut
+
+sub param {
+    my ( $meta, $name, %opts ) = @_;
+
+    $opts{is}       //= 'ro';
+    $opts{required} //= 1;
+
+    # "has [@attributes]" versus "has $attribute"
+    foreach my $attr ( 'ARRAY' eq ref $name ? @$name : $name ) {
+        my %options = %opts;    # copy each time to avoid overwriting
+        $options{init_arg} //= $attr;
+        $meta->add_attribute( $attr, %options );
+    }
+}
+
 =head2 C<field>
 
-Set up our own version of C<field()>.
+    field created => ( isa => PositiveInt, default => sub { time } );
+
+A similar function to Moose's C<has>. A C<field> is never allowed to be passed
+to the constructor, but you can still use C<default> or C<builder>, as normal.
+
+The above C<field> definition is equivalent to:
+
+    has created => (
+        is       => 'ro',
+        isa      => PositiveInt,
+        init_arg => undef,        # not allowed in the constructor
+        default  => sub { time },
+    );
+
+Note that C<field>, like C<param>, defaults to read-only, C<< is => 'ro >>.
+You can override this:
+
+    field some_data => ( is => 'rw', isa => NonEmptyStr );
+
+Otherwise, it behaves like C<has>. You can pass in any arguments that C<has>
+accepts. However, if you pass in C<init_arg>, that will be ignored.
 
 =cut
 
@@ -59,20 +127,6 @@ sub field {
     }
 }
 
-sub param {
-    my ( $meta, $name, %opts ) = @_;
-
-    $opts{is}       //= 'ro';
-    $opts{required} //= 1;
-
-    # "has [@attributes]" versus "has $attribute"
-    foreach my $attr ( 'ARRAY' eq ref $name ? @$name : $name ) {
-        my %options = %opts;    # copy each time to avoid overwriting
-        $options{init_arg} //= $attr;
-        $meta->add_attribute( $attr, %options );
-    }
-}
-
 1;
 
 __END__
@@ -85,8 +139,12 @@ __END__
           qw(compile Num NonEmptyStr Str PositiveInt ArrayRef);
         use List::Util 'sum';
 
+        # the distinction between `param` and `field` makes it easier to
+        # see which are available to `new`
         param _name   => ( isa => NonEmptyStr, init_arg => 'name' );
         param title   => ( isa => Str,         required => 0 );
+
+        # forbidden in the constructor
         field created => ( isa => PositiveInt, default  => sub { time } );
 
         sub name ($self) {
@@ -109,4 +167,40 @@ __END__
 
 =head1 DESCRIPTION
 
-TODO
+This module is B<EXPERIMENTAL>! All features subject to change.
+
+This class attempts to create a "safer" version of Moose that default to
+read-only attributes and is easier to read and write.
+
+It's sort of the equivalent to:
+
+    package My::Class {
+        use v5.22.0;
+        use Moose;
+        use MooseX::StrictConstructor;
+        use feature 'signatures';
+        no warnings 'experimental::signatures';
+        use namespace::autoclean;
+        use Carp;
+        use mro 'c3';
+
+        ... your code here
+    }
+
+It also exports two functions which are similar to Moose C<has>: C<param> and C<field>.
+
+A C<param> is a required parameter (defaults may be used). A C<field> is not
+allowed to be passed to the constructor.
+
+=head1 TODO
+
+    # :(
+    __PACKAGE__->meta->make_immutable; # we want this to be optional
+
+Try to figure out how to automatically make the class immutable.
+C<B::Hooks::EndOfScope> did not work because C<param> and C<field> fire at
+runtime, not compile-time, and making the class immutable at the end of scope
+fires I<before> C<param> and C<field> are run.
+
+I thought seriously about making the class mutable in each of those functions
+and immutable after, but hey, we don't need that performance hit.
