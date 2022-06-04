e5107e426f968f387a708a027311de1764effb98
Ovid <ovid@cpan.org>
2022-06-04 16:13:41 +0200

    Oops

--- lib/MooseX/Extended/Core.pm
+++ lib/MooseX/Extended/Core.pm
@@ -64,7 +64,7 @@ sub _apply_optional_features ( $config, $for_class ) {
             croak("multi subs not supported in Perl version less than v5.26.0. You have $^V");
         }
 
-        use Data : Dumper;
+        use Data::Dumper;
         say STDERR Dumper($config);
         say STDERR Dumper($for_class);
 
