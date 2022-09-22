c2e6f5f71a4918a25dd5f3552f7ffa51d63f51d4
Ovid <ovid@cpan.org>
2022-09-22 08:31:26 +0200

    Make it a touch easier to use types

--- lib/MooseX/Extended/Types.pm
+++ lib/MooseX/Extended/Types.pm
@@ -6,7 +6,22 @@ use strict;
 use warnings;
 use Type::Library -base;
 use Type::Utils -all;
-use Type::Params;    # this gets us compile and compile_named
+
+# there is no :all, so we need to hardcode the list
+use Type::Params qw(
+  compile
+  compile_named
+  multisig
+  validate
+  validate_named
+  compile_named_oo
+  Invocant
+  wrap_subs
+  wrap_methods
+  ArgsObject
+);
+
+# EXPORT_OK, but not :all
 use Types::Standard qw(
   slurpy
 );
@@ -21,11 +36,14 @@ BEGIN {
       Types::Common::String
     );
     push @EXPORT_OK => (
-        'compile',          # from Type::Params
-        'compile_named',    # from Type::Params
-        'slurpy',
+        @Type::Params::EXPORT, @Type::Params::EXPORT_OK,
     );
-    our %EXPORT_TAGS = ( all => \@EXPORT_OK );
+    our %EXPORT_TAGS = (
+        all      => \@EXPORT_OK,
+        Standard => [ Types::Standard->type_names ],
+        Numeric  => [ qw/Num Int Bool/, Types::Common::Numeric->type_names ],
+        String   => [ qw/Str/,          Types::Common::String->type_names ],
+    );
 }
 
 1;
@@ -79,9 +97,48 @@ We automatically include the types from the following:
 
 =item * L<Types::Standard>
 
+You can import them individually or with the C<:Standard> tag:
+
+    use MooseX::Extended::Types types => 'Str';
+    use MooseX::Extended::Types types => [ 'Str', 'ArrayRef' ];
+    use MooseX::Extended::Types types => ':Standard';
+
+Using the C<:Standard> tag is equivalent to:
+
+    use Types::Standard;
+
+No import list is supplied directly to the module, so non-default type
+functions must be asked for by name.
+
 =item * L<Types::Common::Numeric>
 
+You can import them individually or with the C<:Numeric> tag:
+
+    use MooseX::Extended::Types types => 'Int';
+    use MooseX::Extended::Types types => [ 'Int', 'NegativeOrZeroNum' ];
+    use MooseX::Extended::Types types => ':Numeric';
+
+Using the C<:Numeric> tag is equivalent to:
+
+    use Types::Common::Numeric;
+
+No import list is supplied directly to the module, so non-default type
+functions must be asked for by name.
+
 =item * L<Types::Common::String>
+
+You can import them individually or with the C<:String> tag:
+
+    use MooseX::Extended::Types types => 'NonEmptyStr';
+    use MooseX::Extended::Types types => [ 'NonEmptyStr', 'UpperCaseStr' ];
+    use MooseX::Extended::Types types => ':String';
+
+Using the C<:String> tag is equivalent to:
+
+    use Types::Common::String;
+
+No import list is supplied directly to the module, so non-default type
+functions must be asked for by name.
 
 =back
 
@@ -98,6 +155,39 @@ See L<Type::Params>
 =item * C<compile_named>
 
 See L<Type::Params>
+
+=item * C<multisig>
+
+See L<Type::Params>
+
+=item * C<validate>
+
+See L<Type::Params>
+
+=item * C<validate_named>
+
+See L<Type::Params>
+
+=item * C<compile_named_oo>
+
+See L<Type::Params>
+
+=item * C<Invocant>
+
+See L<Type::Params>
+
+=item * C<wrap_subs>
+
+See L<Type::Params>
+
+=item * C<wrap_methods>
+
+See L<Type::Params>
+
+=item * C<ArgsObject>
+
+See L<Type::Params>
+
 
 =item * C<slurpy>
 
